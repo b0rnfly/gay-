9b4b51bb0201aee53199fa638aa07bd3c6937c5b
gregory <34366772+b0rnfly@users.noreply.github.com>
2018-09-25 19:56:43 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1> <span> bonjour, Im Greg and I'm a developer </span></h1>
+    <p> & this is my world</p>
+    <Link to="/page-2/">social streams </Link>
   </div>
 )
 
