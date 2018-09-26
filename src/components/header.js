8d422a2f24f98cf5995827efa8f6e3280b6b5853
gregory <34366772+b0rnfly@users.noreply.github.com>
2018-09-25 20:21:22 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Header = ({ greg/orio}) => (
-  <div
-    style={{
+const Header = ({ siteTitle}) => (
+ 
+   <div 
+  style={{
       background: 'rebeccapurple',
       marginBottom: '1.45rem',
     }}
